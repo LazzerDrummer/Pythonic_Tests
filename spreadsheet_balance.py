c8d2c276f8e2887ab61818e64bc017170bfc3601
LazzerDrummer <lazerdrummer@gmail.com>
2018-04-19 18:04:02 +0300

    Balance: - added nullify() - added delete_latest_cell - correct unseen bug where the lastest value wouldn't change as you inserted or deleted cells -Spreadsheet now only has the rows and cols it needs, and I added .add_rows and .add_cols where needed so it can work with it.
    -add a check_null_row that checks whether a row has any values in it or
    not.
    -drastically improve user friendliness when creating a new Balance
    object, and then implement a default value to column_index.

--- spreadsheet_balance.py
+++ spreadsheet_balance.py
@@ -9,7 +9,6 @@ client = gspread.authorize(creds)
 
 pp = pprint.PrettyPrinter()
 sheet = client.open('Balance').sheet1
-
 # def change_order(first_row, last_row):
 # for i in range(first_row, last_row + 1):
 # values_last_row = sheet.row_values(last_row)
@@ -18,24 +17,30 @@ sheet = client.open('Balance').sheet1
 
 
 class Balance:
-    import gspread
-
+    # import gspread
     def __init__(self, name, column_index):
         self.name = name
         self.column_index = column_index
-        if(sheet.cell(2, column_index).value):
+        if name not in sheet.row_values(1):
+            if(sheet.col_count < column_index):
+                sheet.add_cols(1)
+                sheet.update_cell(1, column_index, name)
+                self.latest_value = 0
+                self.len_this_col = 0
+            else:
+                print("This column is already in use")
+        else:
+            print("This name is already used")
             self.latest_value = int(sheet.cell(2, column_index).value)
-        else:
-            self.latest_value = 0
-        if (name not in sheet.row_values(1)
-                and sheet.cell(1, column_index).value is ""):
-            sheet.update_cell(1, column_index, name)
-        else:
-            print(
-                "This column is already occupied or the name is already used")
+            self.len_this_col = len(sheet.col_values(self.column_index))
+
+    def update_lastest_value(self):
+        self.latest_value = int(sheet.cell(2, self.column_index).value)
 
     def insert_cell(self, value):
-        for i in range(len(sheet.col_values(self.column_index)), 1, -1):
+        if sheet.cell(sheet.row_count, self.column_index).value is not "":
+            sheet.add_rows(1)
+        for i in range(self.len_this_col, 1, -1):
             current_cell = sheet.cell(i, self.column_index).value
             sheet.update_cell(i + 1, self.column_index, current_cell)
         sheet.update_cell(2, self.column_index, value)
@@ -43,10 +48,20 @@ class Balance:
     def change_balance(self, money_value):
         new_value = self.latest_value + money_value
         self.insert_cell(new_value)
+        self.update_lastest_value()
+
+    def nullify(self):
+        self.insert_cell(0)
+
+    def delete_latest_cell(self):
+        for i in range(3, self.len_this_col + 1):
+            current_cell = sheet.cell(i, self.column_index).value
+            sheet.update_cell(i - 1, self.column_index, current_cell)
+        sheet.update_cell(self.len_this_col, self.column_index, "")
+        self.update_lastest_value()
 
 
-cipi = Balance("Cipi", 1)
-stefan = Balance("Stefan", 2)
-alex = Balance("Alex", 3)
-
-alex.change_balance(50)
+# cipi = Balance("Cipi", 2)
+# stefan = Balance("Stefan", 3)
+# alex = Balance("Alex", 4)
+# paul = Balance("Paul", 5)
